56e2d019108de73c0fe9150d462204c0ff86788c
Tommy Evans <webdesign.minhbao@gmail.com>
2025-02-10 19:44:13 +0700

    add responsive layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">Hello World</div>
+  );
 };
 
 export default App;
