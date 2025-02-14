6d0ae963f95f64d8d96a89da1397f1a8d978a93a
Tommy Evans <webdesign.minhbao@gmail.com>
2025-02-14 11:37:08 +0700

    add mongo connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+import connectDB from "./lib/connectDB.js";
 import userRouter from "./routes/user.route.js";
 import commentRouter from "./routes/comments.route.js";
 import postRouter from "./routes/post.route.js";
@@ -7,7 +8,10 @@ import postRouter from "./routes/post.route.js";
 const app = express();
 
 app.use("/users", userRouter);
+app.use("/comments", commentRouter);
+app.use("/posts", postRouter);
 
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running!!!");
 });
